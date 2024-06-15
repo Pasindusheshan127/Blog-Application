f48da94877f5629b31c736953d7d6f1ea0579a4d
Pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-06-15 17:50:07 +0530

    Added dynamic article routes and content creating and linking the article list

--- frontend/src/pages/ArticleList.js
+++ frontend/src/pages/ArticleList.js
@@ -1,10 +1,48 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import articlecontent from "./Article-content";
 
 const ArticleList = () => {
   return (
-    <h1 className="my-6 text-2xl font-bold text-gray-900 sm:text-4xl">
-      Articles
-    </h1>
+    <div>
+      <h1 className="my-6 text-2xl font-bold text-gray-900 sm:text-4xl">
+        Articles
+      </h1>
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {articlecontent.map((article, index) => (
+            <div key={index} className="p-4 md:w-1/2">
+              <div className="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+                <Link to={`/article/${article.name}`}>
+                  <img
+                    className="object-cover object-center w-full lg:h-48 md:h-36"
+                    src={article.thumbnail}
+                    alt="blog"
+                  />
+                </Link>
+                <div className="p-6">
+                  <Link key={index} to={`/article/${article.name}`}>
+                    <h3 className="mb-3 text-lg font-medium text-gray-900">
+                      {" "}
+                      {article.title}
+                    </h3>
+                  </Link>
+                  <p>{article.content[0].substring(0, 110)}...</p>
+                  <div>
+                    <Link
+                      className="inline-flex items-center mt-3 text-indigo-500  md:mb-2 lg:mb-0"
+                      to={`/article/${article.name}`}
+                    >
+                      {" "}
+                      Learn More
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
